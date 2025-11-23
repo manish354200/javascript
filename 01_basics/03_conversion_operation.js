ee634983472237a83da92a8b08f9ad86e6936738
manish354200 <my562863@gmail.com>
2025-11-23 06:42:36 +0000

    Conversion

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,43 @@
+let value=3
+let negvalue=-value
+//console. log(negValue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2*2)
+//console.log(2**2) // ** ka meaning 2 ki power 2...
+//console.log(2%3)
+
+let str1="hello"
+let str2=" manish"
+
+let str3=str1 + str2
+console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2); // it's all derive from ECMA script guidelines
+//console.log(1 + 2 + "2");
+
+//console.log(3 + 4 * 5 % 3); // here please use parenthesis()....
+
+//console.log(true); output true
+//console.log(+true); output 1 because true is boolean but true+ not possible
+console.log(+""); // output 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not a good way to degine vlaue 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // read prefix and postfix from (prefix and postfix js mdn)...
+
+
+
+
+
+
+
+
