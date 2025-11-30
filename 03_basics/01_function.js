407486b6460c7b293c12ca64bc0b8d3bc07aab00
manish354200 <my562863@gmail.com>
2025-11-30 20:51:36 +0000

    functions basic

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,11 +1,11 @@
 
 function sayMyName(){
-    console.log("M")
-    console.log("a")
-    console.log("n")
-    console.log("i")
-    console.log("s")
-    console.log("h")
+    // console.log("M")
+    // console.log("a")
+    // console.log("n")
+    // console.log("i")
+    // console.log("s")
+    // console.log("h")
 }
 
 // sayMyName() // sayMyName is function referernce and sayMyName() is function execution..
@@ -25,10 +25,43 @@ const result = addTwoNumbers(3, 5)
 
 function loginUserMessage(username){
     if(!username){
-        console.log("Please enter a username")
+        //console.log("Please enter a username")
         return
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage()) // if doesn't pass any value it shows usefined user just logged in.
+//console.log(loginUserMessage()) // if doesn't pass any value it shows usefined user just logged in.
+
+
+function calculateCartPrice(...num1){//... before num1 is rest operator
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500))
+
+const  user ={
+    username:"Manish",
+    price: 89
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is
+         ${anyobject.price}`);
+}
+
+//handleObject(user) to run the above function
+
+handleObject({
+    username:"ram",
+    price:768
+})
+
+const myNewArray =[200, 400, 100, 600]
+
+function returnSecondValue(gatArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
