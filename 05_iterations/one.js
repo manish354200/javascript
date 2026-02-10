5f135241add34faf96f4161b40bedaa125cf36ef
manish354200 <my562863@gmail.com>
2026-02-10 15:42:19 +0000

    for loop in js

---
+++ 05_iterations/one.js
@@ -0,0 +1,35 @@
+// for
+//console.log("manish")
+for(let i=0;i<=10;i++){
+  const element =i
+  if(element == 5){
+    //console.log("5 is best number");
+  }
+  //console.log(element)
+}
+
+for(let i=0;i<=10;i++){
+  //console.log(`Ourter loop value ${i}`)
+  for(let j=0;j<10;j++){
+     //console.log(`Inner loop value ${j} and inner loop value ${i})
+     //console.log(i+`*`+j+`=`+i*j)
+  }
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+for(let i = 0;i<myArray.length; i++){
+  const element =myArray[i];
+  //console.log(element);
+}
+
+
+// break and continue(ek baar maaf karna)
+
+for(let i = 0;i<=20; i++){
+  if(i==5){ 
+  //console.log(`value of i is ${i}`);
+  break
+  }
+  //console.log(`Value of i is ${i}`)
+}
