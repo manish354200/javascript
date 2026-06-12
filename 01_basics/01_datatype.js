b7a8adae3e7ab2ef41e11db439c7e37fe8d0944d
Manish yadav <my562863@gmail.com>
2026-06-12 17:45:07 +0000

    change

--- 01_basics/01_datatype.js
+++ 01_basics/01_datatype.js
@@ -1,7 +1,7 @@
 "use strict"; //treat all JS code as newer version
 // alert (3+3) //we are using nodejs, not browser
 
-console.log(3+3)   //code readability should be high ( use referece of ecmascript international,tce39 and mdn)
+console.log(3+3)   //code readability should be high ( use referece of ECMascript international,tce39 and mdn)
 console.log("Manish")
 let name="manish"
 let age=18
